e5119e1b9b5afa6d349a0f0284584a7da0b79631
Gabor Borbely <gaebor@gmail.com>
2019-02-02 17:45:19 +0100

    idk

--- guineacam.sh
+++ guineacam.sh
@@ -10,10 +10,10 @@ IP_FILE=~/ip.txt
 while true
 do
 ### ip
-  ip_str=`lynx -connect_timeout=10 --dump http://ipecho.net/plain 2> /dev/null | grep -P "(\d{1,3}\.){3}\d{1,3}"`
-  if [[ $? -eq 0 && $ip_str ]]
+  ip_str=`lynx -connect_timeout=10 --dump http://ipecho.net/plain 2> /dev/null | grep -P "(\d{1,3}\.){3}\d{1,3}" | tr -d '[:space:]'`
+  if [ ( $? -eq 0 ) -a ( -n "$ip_str" ) ]
   then
-    if [[ ! -e $IP_FILE || "`cat $IP_FILE`" != $ip_str ]]
+    if [ ( ! -e $IP_FILE ) -o ( "`cat $IP_FILE`" != "$ip_str" ) ]
     then
       echo $ip_str > $IP_FILE
       echo "http://$ip_str/
@@ -28,8 +28,8 @@ rtsp://$ip_str:8554/" | m "ip changed"
   sudo raspivid -o - -t 1000000 -w 480 -h 640 --rotation 90 --metering average --profile high --nopreview -fps 25 --intra 25 | \
   cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 --play-and-exit
   VIDEO_RESULT=$?
-### check error  
-  if [ $STILL_RESULT != 0 ] || [ $VIDEO_RESULT != 0 ] || [ -n "$STILL_ERROR" ]
+### check error
+  if [ ( $STILL_RESULT -ne 0 ) -o ($VIDEO_RESULT -ne 0 ) -o ( -n "$STILL_ERROR" ) ]
   then
     m "camera error" <<<"\`raspistill\` returned $STILL_RESULT and reported:
 $STILL_ERROR
